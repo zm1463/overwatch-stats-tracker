bec95c4b5c13ad1a3d4606739a5edec534831db4
Zishawn Maajed <zmaajed@hotmail.com>
2026-06-13 17:51:26 -0500

    fix: resolved backend CORS policy, and corrected the URL that was being fetched

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,26 @@ from fastapi import FastAPI, Depends, HTTPException
 from typing import List
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
+
 
 import models
 from database import engine, SessionLocal
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173,"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 models.Base.metadata.create_all(bind = engine)
 
 
